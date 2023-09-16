d36eea790af5fd347a3f55423007e8ec156b4daa
hersvin13 <87289544+hersvin13@users.noreply.github.com>
2023-09-16 17:58:41 +0800

    modals

---
+++ Frontend/src/modules/organization/sub-modules/modal/branch-modal/add-branch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddNewBranch = () => {
+  return (
+    <>
+      <div>AddNewBranch</div>
+    </>
+  );
+};
+
+export default AddNewBranch;
---
+++ Frontend/src/modules/organization/sub-modules/modal/branch-modal/update-branch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateBranch = () => {
+  return (
+    <>
+      <div>UpdateBranch</div>
+    </>
+  );
+};
+
+export default UpdateBranch;
